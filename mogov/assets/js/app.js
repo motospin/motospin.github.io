adeda27fe34a87f5285b4c5d5e660f2333017c51
motospin <motospin@users.noreply.github.com>
2020-08-20 18:51:24 +0700

    mogov

--- mogov/assets/js/app.js
+++ mogov/assets/js/app.js
@@ -24,9 +24,50 @@ $(function() {
 
 
     
-    /* Smooth scroll */
+    /* Smooth Scroll */
+    $("[data-scroll]").on("click", function(event){
+        event.preventDefault();
 
+        var $this = $(this)
+            blockId = $this.data('scroll');
+            blockOffset = $(blockId).offset().top;
 
+        $("#nav a").removeClass("active");
+
+        $("#nav").removeClass("active");
+        $("#nav_toggle").removeClass("active");
+
+       $("html, body").animate({
+            scrollTop: blockOffset - 69
+
+       }, 500);
+
+    });
 
 });
 
+/* Nav toggle */
+$("#nav_toggle").on("click", function(event){
+    event.preventDefault();
+    
+    $("#nav_toggle").toggleClass("active");
+    $("#nav").toggleClass("active");
+   });
+
+/* Collapse */
+$("[data-collapse").on("click", function(event){
+    event.preventDefault();
+
+    var $this = $(this)
+        blockId = $this.data('collapse');
+
+    $this.toggleClass("active");
+});
+
+/* Slider */
+$("[data-slider]").slick({
+    infinitie: true,
+    fade: false,
+    slideToShow: 1,
+    slideToScroll: 1
+});
